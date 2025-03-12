5908561e9259a6e10f345ab6a8b53c296d10f9c3
juniyasyos <juniyasyos@gmail.com>
2025-03-12 10:27:37 +0700

    fix translate, fix name authentication settings

--- src/Pages/AuthenticationSettings.php
+++ src/Pages/AuthenticationSettings.php
@@ -3,7 +3,6 @@
 namespace Juniyasyos\FilamentSettingsHub\Pages;
 
 use Juniyasyos\FilamentSettingsHub\Settings\KaidoSetting;
-// use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Juniyasyos\FilamentSettingsHub\Traits\UseShield;
 use Filament\Forms;
 use Filament\Forms\Components\Section;
@@ -12,13 +11,11 @@ use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
 
-class ManageSettingKaido extends SettingsPage
+class AuthenticationSettings extends SettingsPage
 {
-    // use HasPageShield;
     use UseShield;
 
     protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
-
     protected static string $settings = KaidoSetting::class;
 
     public static function shouldRegisterNavigation(): bool
@@ -26,24 +23,24 @@ class ManageSettingKaido extends SettingsPage
         return false;
     }
 
-
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Section::make('Site Information')->columns(1)->schema([
+        return $form->schema([
+            Section::make(__('filament-settings-hub::messages.settings.authentication.section_title'))
+                ->columns(1)
+                ->schema([
                     TextInput::make('site_name')
-                        ->label('Site Name')
+                        ->label(__('filament-settings-hub::messages.settings.authentication.form.site_name'))
                         ->required(),
                     Toggle::make('site_active')
-                        ->label('Site Active'),
+                        ->label(__('filament-settings-hub::messages.settings.authentication.form.site_active')),
                     Toggle::make('registration_enabled')
-                        ->label('Registration Enabled'),
+                        ->label(__('filament-settings-hub::messages.settings.authentication.form.registration_enabled')),
                     Toggle::make('password_reset_enabled')
-                        ->label('Password Reset Enabled'),
+                        ->label(__('filament-settings-hub::messages.settings.authentication.form.password_reset_enabled')),
                     Toggle::make('sso_enabled')
-                        ->label('SSO Enabled'),
+                        ->label(__('filament-settings-hub::messages.settings.authentication.form.sso_enabled')),
                 ]),
-            ]);
+        ]);
     }
 }
