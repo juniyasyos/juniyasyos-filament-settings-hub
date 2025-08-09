ea902c8259c32e1150c94568620b2e063bfabdbf
Ahmad Ilyas <111669845+juniyasyos@users.noreply.github.com>
2025-08-09 12:02:40 +0700

    refactor settings plugin and tidy pages

--- src/Pages/SocialMenuSettings.php
+++ src/Pages/SocialMenuSettings.php
@@ -5,7 +5,6 @@ namespace Juniyasyos\FilamentSettingsHub\Pages;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\TextInput;
-use Filament\Pages\Actions\Action;
 use Filament\Pages\SettingsPage;
 use Juniyasyos\FilamentSettingsHub\Settings\SitesSettings;
 use Juniyasyos\FilamentSettingsHub\Traits\HasSettingsBreadcrumbs;
@@ -13,7 +12,8 @@ use Juniyasyos\FilamentSettingsHub\Traits\UseShield;
 
 class SocialMenuSettings extends SettingsPage
 {
-    use HasSettingsBreadcrumbs, UseShield;
+    use HasSettingsBreadcrumbs;
+    use UseShield;
 
     protected static ?string $navigationIcon = 'heroicon-o-cog';
 
@@ -34,20 +34,6 @@ class SocialMenuSettings extends SettingsPage
             'Halaman ini tidak tersedia.'
         );
     }
-
-    // protected function getActions(): array
-    // {
-    //     $tenant = \Filament\Facades\Filament::getTenant();
-    //     if ($tenant) {
-    //         return [
-    //             Action::make('back')->action(fn () => redirect()->route('filament.'.filament()->getCurrentPanel()->getId().'.pages.settings-hub', $tenant))->color('danger')->label(trans('filament-settings-hub::messages.back')),
-    //         ];
-    //     }
-
-    //     return [
-    //         Action::make('back')->action(fn () => redirect()->route('filament.'.filament()->getCurrentPanel()->getId().'.pages.settings-hub'))->color('danger')->label(trans('filament-settings-hub::messages.back')),
-    //     ];
-    // }
 
     public static function shouldRegisterNavigation(): bool
     {
